7be0a0d55f6de8a5757ca18f700fc13cd00c01b0
Ru-Rus <marquilleroruselljohn@gmail.com>
2026-02-11 22:48:56 +0800

    "p"

--- server.js
+++ server.js
@@ -8,11 +8,14 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+
+// Serve static files from Public folder
+const publicPath = path.join(__dirname, "Public");
+app.use(express.static(publicPath));
 
 // Root route - serve index.html
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.sendFile(path.join(publicPath, "index.html"));
 });
 
 // API to unlock and get letter
